ad23613dd14c6a86468ebf8b7bc8febf5bd6cf94
ETEMESI <samweletemesi2@Gmail.com>
2024-01-05 22:02:29 +0300

    corrected assignment after review.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+
+// const companyName = 'Scubar';
+console.log(companyName);
+
+let mostProfitableNeighborhood = 'Chelsea';
+console.log(mostProfitableNeighborhood);
+
+let companyCeo = 'Susan Smith';
+console.log(companyCeo);
